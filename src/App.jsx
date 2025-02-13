2d6cd586e9e9b2f440216f003ce1da7dc8d8792a
dmi <dmi@gmail.com>
2025-02-13 06:33:05 -0800

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import TicketSelection from './Components/TicketSelection';
 import './App.css';
+import { Routes, Route } from "react-router-dom"
 import AttendeeDetails from './Components/AttendeeDetails';
 import TicketConfirmation from './Components/TicketConfirmation';
 import Navbar from './Components/Navbar';
@@ -9,9 +10,14 @@ function App() {
   return (
     <div className="app">
       <Navbar />
-      <TicketSelection />
-      <AttendeeDetails />
-      <TicketConfirmation />
+      <Routes>
+        <Route path='/' element={<TicketSelection />} />
+        {/* <Route path='/tickets' element={} /> */}
+        {/* <Route path='/about-project' element={} /> */}
+      </Routes>
+      {/* <TicketSelection /> */}
+      {/* <AttendeeDetails /> */}
+      {/* <TicketConfirmation /> */}
     </div>
   );
 }
